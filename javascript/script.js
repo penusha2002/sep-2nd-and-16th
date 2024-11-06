304d4416aedda686df9b820cff01c92a287eb223
Prasanth Kumar Nagothi <nprasanth4046@gmail.com>
2024-11-06 15:39:15 +0530

    Javascript introduction

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+// Javascript is a dynamic language
+
+// Print the values using console.log()
+console.log("Hello, Welcome to JS");
+console.log("Hello, 2nd line");
+
+// Varibale is name which given to a memory location where we store our data
+// Variables will created by using const, let, var.
+let a = "prasanth"
+console.log("a");
