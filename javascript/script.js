ba57ec8ed4ff8dcde27e86244328a0e0397881fe
Prasanth Kumar Nagothi <nprasanth4046@gmail.com>
2024-11-07 16:03:33 +0530

    Javascript Variables, Datatypes and Operators

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,52 @@
 // Javascript is a dynamic language
 
 // Print the values using console.log()
-console.log("Hello, Welcome to JS");
-console.log("Hello, 2nd line");
+// console.log("Hello, Welcome to JS");
+// console.log("Hello, 2nd line");
 
 // Varibale is name which given to a memory location where we store our data
 // Variables will created by using const, let, var.
-let a = "prasanth"
-console.log("a");
+const studentId = 7643746;
+let studentName = "Hemanth";
+var studentEmail = "hemanth@google.com";
+let studentMobile = null;
+let isVerified = false
+
+
+// studentId = "736437" //Assignment is not allwoed for const varibales
+// studentName = "Manoj"
+// studentEmail = "manoj@gmail.com"
+// studentMobile = 123
+
+// console.table([studentId, studentEmail, studentName, studentMobile])
+
+
+/*
+Datatypes
+
+1. Number
+2. String
+3. Boolean
+4. Null
+5. Undefined
+*/
+
+let langName = "JavaScript"
+
+// String Interpolation
+// console.log(`I like ${langName} language`);
+
+
+// Arithematic operators --> +,-,*,/,%,**
+
+// Relational Operators --> <, >, <=, >=, ==, ===, !=
+
+// Logical Operators --> &&, ||, !
+
+// Assignment Operator --> =, +=, -=, *=, /= ...
+
+let a = 10
+let b = 20
+a += b // a = a + b
+console.log(a);
+
